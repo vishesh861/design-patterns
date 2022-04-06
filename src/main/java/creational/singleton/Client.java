1dae6666f792700cab68a211069ed5392e48dc41
vishesh861 <vishesh861994@gmail.com>
2022-04-07 00:04:03 +0530

    singleton

---
+++ src/main/java/creational/singleton/Client.java
@@ -0,0 +1,14 @@
+package creational.singleton;
+
+public class Client {
+
+	public static void main(String[] args) {
+		LazyRegistryIODH singleton;
+		
+		
+		singleton = LazyRegistryIODH.getInstance();
+		LazyRegistryIODH singleton2 = LazyRegistryIODH.getInstance();
+		System.out.println(singleton == singleton2);
+	}
+
+}
