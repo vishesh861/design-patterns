01d3331bc9f66e852acd3ef813992653b0290d4f
vishesh861 <vishesh861994@gmail.com>
2022-04-07 00:18:10 +0530

    structural

---
+++ src/main/java/structural/adapter/Customer.java
@@ -0,0 +1,14 @@
+package structural.adapter;
+
+/**
+ * Target interface required by new client code
+ * Target Interface
+ */
+public interface Customer {
+
+	String getName();
+	
+	String getDesignation();
+	
+	String getAddress();
+}
