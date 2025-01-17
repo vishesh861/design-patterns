f3696bced770adf92ad33593e747e4e5ce937ece
v0a02it <Vishesh.Agarwal@walmart.com>
2025-01-18 00:00:34 +0530

    Decorator and composite design pattern

---
+++ src/main/java/structural/decorator/HtmlEncodedMessage.java
@@ -0,0 +1,20 @@
+package structural.decorator;
+
+import org.apache.commons.text.StringEscapeUtils;
+
+//Decorator. Implements component interface
+public class HtmlEncodedMessage implements Message {
+
+	private Message msg;
+	
+	public HtmlEncodedMessage(Message msg) {
+		this.msg = msg;
+	}
+	
+	@Override
+	public String getContent() {
+		return StringEscapeUtils.escapeHtml4(msg.getContent());
+	}
+
+	
+}
