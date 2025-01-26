796176d6936f553ad6d4b25b12701fa743256afd
v0a02it <Vishesh.Agarwal@walmart.com>
2025-01-26 16:21:39 +0530

    ObjectPool

---
+++ src/main/java/creational/objectpool/Image.java
@@ -0,0 +1,11 @@
+package creational.objectpool;
+
+//Represents our abstract reusable
+public interface Image extends Poolable{
+
+    void draw();
+
+    Point2D getLocation();
+
+    void setLocation(Point2D location);
+}
